825311f6a9a498d4acfc048e7a8831d63e63b66b
chengetanai <mukanhairic@gmail.com>
2024-04-11 12:50:27 +0200

    refactor error page

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -10,11 +10,16 @@ export function ErrorPage(){
                 <h1 className="text-xl text-slate-900">
                     Sorry, an error has occured
                 </h1>
+                {isError(error) && (
+                    <p className="text-base text-slate-700">{error.statusText}</p>
+                )}
             </div>
         </>
     )
 }
 
-function isError(error:any){
-    
+function isError(error:any):error is {
+    statusText: string
+}{
+    return "statusText" in error;
 }
